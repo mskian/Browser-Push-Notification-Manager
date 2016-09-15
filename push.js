e87523c6d6abef833f8e29a672a2cc73eb76cb98
Santhosh veer <mskian@users.noreply.github.com>
2016-09-15 20:58:51 +0530

    Update push.js

--- push.js
+++ push.js
@@ -1,15 +1,16 @@
 <script>
+//Replace YOUR TITLE and YOUR LINK with your Post title and post link
 var articles = [
-["Google URL Shortener Web App.","http://www.allwebtuts.com/google-url-shortener-web-app/"],
-["My HTML post Widgets (My Wordpress Plugin).","https://wordpress.org/plugins/my-html-post-widgets/"],
-["My Bitly PHP Web App","http://www.allwebtuts.com/bitly-web-app/"],
-["add the Stylish Headings in WordPress without plugins","http://www.allwebtuts.com/add-the-stylish-headings-in-wordpress-without-plugins/"],
-["Allwebtuts Lean Wordpress","http://www.allwebtuts.com"],
-["Powerful password Generator.","https://www.mskian.com/tools/imgencode"],
-["Blogger Sitemap Generator","https://www.mskian.com/tools/bloggersitemap"],
-["Base64 image Encoder.","https://www.mskian.com/tools/imgencode"],
-["Feedburner Email subscriber booster plugin.","http://www.allwebtuts.com/feedburner-email-subscriber-booster/"],
-["Facebook Messenger Contact Form.","http://www.allwebtuts.com/facebook-messenger-contact-form/"]
+["YOUR TITLE","YOUR LINK"], 
+["YOUR TITLE","YOUR LINK"],
+["YOUR TITLE","YOUR LINK"],
+["YOUR TITLE","YOUR LINK"],
+["YOUR TITLE","YOUR LINK"],
+["YOUR TITLE","YOUR LINK"],
+["YOUR TITLE","YOUR LINK"],
+["YOUR TITLE","YOUR LINK"],
+["YOUR TITLE","YOUR LINK"],
+["YOUR TITLE","YOUR LINK"],
 ];
 
 
@@ -19,7 +20,7 @@ var title=articles[x][0];
 var desc='Most popular article.';
 var url=articles[x][1];
 notifyBrowser(title,desc,url);
-}, 150000);
+}, 120000);  //calls every 2 minutes 
 
 
 
@@ -55,7 +56,7 @@ Notification.requestPermission();
 }
 else {
 var notification = new Notification(title, {
-icon:'https://www.mskian.com/assets/mskbell.png',
+icon:'Your Notification ICON URL', //replace with your Notification URL
 body: desc,
 });
 
